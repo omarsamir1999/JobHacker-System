3e67ccbcb3745e5cb73453104a7bd33321e77773
Omar Samir <omarsamir430@gmail.com>
2023-04-19 01:04:47 +0200

    finished tasks week 4

--- week 4/src/PalindromeChecker.java
+++ week 4/src/PalindromeChecker.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class PalindromeChecker {
+import java.util.Scanner;
+
+public class PalindromeChecker {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+        if (isPalindrome(input)) {
+            System.out.println(input + " is a palindrome");
+        } else {
+            System.out.println(input + " is not a palindrome");
+        }
+    }
+
+    public static boolean isPalindrome(String inputString) {
+        String reversed = "";
+        for (int i = inputString.length() - 1; i >= 0; i--) {
+            reversed += inputString.charAt(i);
+        }
+        return inputString.equals(reversed);
+    }
 }
--- week 4/src/ReverseString.java
+++ week 4/src/ReverseString.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class ReverseString {
+import java.util.Scanner;
+
+public class ReverseString {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String inputString  = scanner.nextLine();
+        String reversedStr = "";
+
+        for (int i = inputString .length() - 1; i >= 0; i--) {
+            reversedStr += inputString.charAt(i);
+        }
+
+        System.out.println("Reversed string: " + reversedStr);
+    }
 }
